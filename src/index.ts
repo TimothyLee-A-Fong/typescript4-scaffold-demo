696d7f9e8ca746f3b9d4329104e3efdaa1a3ee50
Timothy Lee-A-Fong <timothy.lee-a-fong@sanoma.com>
2023-04-26 00:32:55 +0200

    TypeScript Compiler Configuration

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
+import { Main } from "./app/main";
+
 // TypeScript is highly efficient.
 const message: string = "Hello TypeScript!";
 
 console.log(message);
+
+new Main().render();
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.ts',
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/, // tsx only necessary for react projects
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    }
+    ,
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'bin')
+    },
+    devServer: {
+        static: [path.join(__dirname, 'public'), path.join(__dirname, 'bin')],
+        port: 7777
+    }
+}
